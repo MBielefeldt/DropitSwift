d160d929c205d0f0d2645e5f12099c9690a4ea3e
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2015-06-04 00:39:01 +0200

    Drops added in the top of the gameView

--- DropitSwift/DropitViewController.swift
+++ DropitSwift/DropitViewController.swift
@@ -10,4 +10,50 @@ import UIKit
 
 class DropitViewController: UIViewController
 {
+    @IBOutlet weak var gameView: UIView!
+    
+    var dropsPerRow = 10
+    
+    var dropSize: CGSize {
+        let size = gameView.bounds.size.width / CGFloat(dropsPerRow)
+        return CGSize(width: size, height: size)
+    }
+    
+    @IBAction func drop(sender: UITapGestureRecognizer)
+    {
+        drop()
+    }
+    
+    private func drop()
+    {
+        var frame = CGRect(origin: CGPointZero, size: dropSize)
+        frame.origin.x = CGFloat.random(dropsPerRow) * dropSize.width
+        
+        let dropView = UIView(frame: frame)
+        dropView.backgroundColor = UIColor.random
+        
+        gameView.addSubview(dropView)
+    }
+}
+
+private extension CGFloat
+{
+    static func random(max: Int) -> CGFloat
+    {
+        return CGFloat(arc4random() % UInt32(max))
+    }
+}
+
+private extension UIColor
+{
+    class var random: UIColor {
+        switch arc4random() % 5 {
+            case 0:  return UIColor.greenColor()
+            case 1:  return UIColor.blueColor()
+            case 2:  return UIColor.orangeColor()
+            case 3:  return UIColor.redColor()
+            case 4:  return UIColor.purpleColor()
+            default: return UIColor.greenColor()
+        }
+    }
 }
