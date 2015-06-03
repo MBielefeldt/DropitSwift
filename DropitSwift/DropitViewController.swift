a3076fcbf341903c5760576193bbfd8f969c30ae
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2015-06-03 23:36:35 +0200

    Initial commit

---
+++ DropitSwift/DropitViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DropitViewController.swift
+//  DropitSwift
+//
+//  Created by Mads Bielefeldt on 03/06/15.
+//  Copyright (c) 2015 Mads Bielefeldt. All rights reserved.
+//
+
+import UIKit
+
+class DropitViewController: UIViewController
+{
+}
